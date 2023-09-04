8cee62bc3d320c3277d9abb9e582b23f9a523131
ghaskari <g.askari1037@gmail.com>
2023-09-04 13:18:19 +0330

    add/ edit main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+from searchresults import *
+
+# Create a search button
+search_button = tk.Button(root, text="Search", command=show_search_results)
+search_button.pack()
+
+# Start the main event loop
+root.mainloop()
